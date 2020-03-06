1582893ab113cdaa09c6e55aac41adfe8694c281
Robin Hermann <robin.hermann@gmail.com>
2020-03-06 10:38:51 +0100

    changing code styling

--- PSTools.psd1
+++ PSTools.psd1
@@ -120,4 +120,3 @@ PrivateData = @{
 # DefaultCommandPrefix = ''
 
 }
-
--- PSTools.psm1
+++ PSTools.psm1
@@ -1,20 +1,15 @@
 
 #Get public and private function definition files.
-$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+$Public  = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
+$Private = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
 
 #Dot source PS Files
-Foreach($import in @($Public + $Private))
-{
-    Try
-    {
+Foreach($import in @($Public + $Private)) {
+    Try {
         . $import.fullname
-    }
-    Catch
-    {
+    } Catch {
         Write-Error -Message "Failed to import function $($import.fullname): $_"
     }
 }
-
 
 Export-ModuleMember -Function $Public.Basename
