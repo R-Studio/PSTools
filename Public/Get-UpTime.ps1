e82c898484cd68a8a54d8773b095ccc3e190e495
WSRW10\Robin Hermann <robin.hermann@gmail.com>
2020-03-07 13:08:08 +0100

    change code format

--- Public/Get-UpTime.ps1
+++ Public/Get-UpTime.ps1
@@ -21,80 +21,86 @@
 Function Get-Uptime {
     [CmdletBinding()]
     param(
-      [parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
-        $ComputerName,
-      [System.Management.Automation.PSCredential]
+        [parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$ComputerName,
+        
+        [System.Management.Automation.PSCredential]
         $Credential
     )
  
     begin {
-      function Out-Object {
-        param(
-          [System.Collections.Hashtable[]] $hashData
-        )
-        $order = @()
-        $result = @{}
-        $hashData | ForEach-Object {
-          $order += ($_.Keys -as [Array])[0]
-          $result += $_
+        function Out-Object {
+            param(
+                [System.Collections.Hashtable[]] $hashData
+            )
+
+            $order = @()
+            $result = @{ }
+            $hashData | ForEach-Object {
+                $order += ($_.Keys -as [Array])[0]
+                $result += $_
+            }
+            New-Object PSObject -Property $result | Select-Object $order
         }
-        New-Object PSObject -Property $result | Select-Object $order
-      }
  
-      function Format-TimeSpan {
-        process {
-          "{0:00}d {1:00}h {2:00}m {3:00}s" -f $_.Days,$_.Hours,$_.Minutes,$_.Seconds
+        function Format-TimeSpan {
+            process {
+                "{0:00}d {1:00}h {2:00}m {3:00}s" -f $_.Days, $_.Hours, $_.Minutes, $_.Seconds
+            }
         }
-      }
  
-      function Get-InnerUptime {
-        param(
-          $ComputerName,
-          $Credential
-        )
-        # In case pipeline input contains ComputerName property
-        if ( $computerName.ComputerName ) {
-          $computerName = $computerName.ComputerName
+        function Get-InnerUptime {
+            param(
+                $ComputerName,
+                $Credential # -> tt have to be not use SecureString
+            )
+
+            # In case pipeline input contains ComputerName property
+            if ($computerName.ComputerName) {
+                $computerName = $computerName.ComputerName
+            }
+
+            if ((-not $computerName) -or ($computerName -eq ".")) {
+                $computerName = [Net.Dns]::GetHostName()
+            }
+
+            $params = @{
+                "Class"        = "Win32_OperatingSystem"
+                "ComputerName" = $computerName
+                "Namespace"    = "root\CIMV2"
+            }
+
+            if ( $credential ) {
+                # Ignore -Credential for current computer
+                if ($computerName -ne [Net.Dns]::GetHostName()) {
+                    $params.Add("Credential", $credential)
+                }
+            }
+
+            try {
+                $wmiOS = Get-WmiObject @params -ErrorAction Stop
+            } catch {
+                Write-Error -Exception (New-Object $_.Exception.GetType().FullName `
+                    ("Cannot connect to the computer '$computerName' due to the following error: '$($_.Exception.Message)'",
+                        $_.Exception))
+                return
+            }
+
+            $lastBootTime = [Management.ManagementDateTimeConverter]::ToDateTime($wmiOS.LastBootUpTime)
+            Out-Object `
+            @{"ComputerName" = $computerName},
+            @{"LastBootTime" = $lastBootTime},
+            @{"Uptime" = (Get-Date) - $lastBootTime | Format-TimeSpan}
         }
-        if ( (-not $computerName) -or ($computerName -eq ".") ) {
-          $computerName = [Net.Dns]::GetHostName()
-        }
-        $params = @{
-          "Class" = "Win32_OperatingSystem"
-          "ComputerName" = $computerName
-          "Namespace" = "root\CIMV2"
-        }
-        if ( $credential ) {
-          # Ignore -Credential for current computer
-          if ( $computerName -ne [Net.Dns]::GetHostName() ) {
-            $params.Add("Credential", $credential)
-          }
-        }
-        try {
-          $wmiOS = Get-WmiObject @params -ErrorAction Stop
-        }
-        catch {
-          Write-Error -Exception (New-Object $_.Exception.GetType().FullName `
-            ("Cannot connect to the computer '$computerName' due to the following error: '$($_.Exception.Message)'",
-            $_.Exception))
-          return
-        }
-        $lastBootTime = [Management.ManagementDateTimeConverter]::ToDateTime($wmiOS.LastBootUpTime)
-        Out-Object `
-          @{"ComputerName" = $computerName},
-          @{"LastBootTime" = $lastBootTime},
-          @{"Uptime"       = (Get-Date) - $lastBootTime | Format-TimeSpan}
-      }
     }
  
     process {
-      if ( $ComputerName ) {
-        foreach ( $computerNameItem in $ComputerName ) {
-            Get-InnerUptime $computerNameItem $Credential
+        if ($ComputerName) {
+            foreach ($computerNameItem in $ComputerName) {
+                Get-InnerUptime $computerNameItem $Credential
+            }
+        } else {
+            Get-InnerUptime "."
         }
-      }
-      else {
-        Get-InnerUptime "."
-      }
     }
 }
